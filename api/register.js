3e3c67fac7ae47613f3b4bbff793bea1560097c5
Vansh Saxena <102627940+vanshisop@users.noreply.github.com>
2024-12-02 16:43:06 -0700

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-#Connect
+
 const pool = new Pool({
   connectionString: 'postgres://postgres.wewkjwtalqhjugzwrcql:_CPGJypV9RU$Nq-@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
 });
