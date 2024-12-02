cc7acbcd34df1675ff2753fe87e2c120942e1684
Vansh Saxena <102627940+vanshisop@users.noreply.github.com>
2024-12-02 16:28:50 -0700

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://postgres.wewkjwtalqhjugzwrcql:_CPGJypV9RU$Nq-@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
+  connectionString: process.ENV.DATABASE_URL,
 });
 
 export default async function handler(req, res) {
