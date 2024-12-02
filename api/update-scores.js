4923488ea340e9fbbcadf96d3b0113f32ca8bb9d
Vansh Saxena <102627940+vanshisop@users.noreply.github.com>
2024-12-02 16:41:21 -0700

    Update update-scores.js

--- api/update-scores.js
+++ api/update-scores.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://postgres.wewkjwtalqhjugzwrcql:_CPGJypV9RU$Nq-@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
 });
 
 export default async function handler(req, res) {
